896dbac5bcf2bb8394753b170165ad89691b06b2
LonelyWind <wxf252577@gmail.com>
2014-01-04 23:55:42 +0800

    11

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -1,5 +1,14 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DB_TYPE'		=>	'mysql',
+	'DB_HOST'		=>	'localhost',
+	'DB_NAME'		=>	'webdesktop',
+	'DB_USER'		=>	'root',
+	'DB_PWD'		=>	'',
+	'DB_PREFIX'		=>	'wd_',
+	'DB_PROT'		=>	3306,
+	'TMPL_L_DELIM'	=>	'<{',
+	'TMPL_R_DELIM'	=>	'}>'
 );
 ?>
